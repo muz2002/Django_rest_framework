39a0e6831a06e55342bb8207c3e9462639c0724f
muz2002 <muzaffar.nazarkhudoev_2026@ucentralasia.org>
2024-05-04 01:53:51 +0600

    Class Based Views

--- posts/urls.py
+++ posts/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 urlpatterns = [
     path("homepage/", homepage, name = 'home_page'),
-    path("list/", list_posts, name = 'list'),
-    path("<int:post_id>", post_detail, name = 'post_detail'),
-    path("update/<int:post_id>", update_post, name = 'update'),
-    path("delete/<int:post_id>",post_delete, name= 'delete')
+    path("list/", PostListCreateView.as_view(), name = 'list'),
+    # path("<int:post_id>", post_detail, name = 'post_detail'),
+    path("<int:post_id>", PostRetrieveUpdateDeleteView.as_view(), name = 'post_detail'),
+    # path("delete/<int:post_id>",post_delete, name= 'delete')
 ]
